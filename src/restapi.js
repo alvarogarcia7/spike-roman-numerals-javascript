be809885c02545a59106a3343ebdb84848cac256
Alvaro Garcia <derecerca@gmail.com>
2016-07-18 15:11:52 +0100

    refactor: extract business logic to another module

--- src/restapi.js
+++ src/restapi.js
@@ -2,12 +2,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var self = this;
 app.use(bodyParser.json());
 
 app.post('/toroman', function(req, res) {
     console.log(req.body);
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ 'roman': 'I' }));
+  res.send(JSON.stringify({ 'roman': self.converter(req.body.arabic) }));
 });
 
-module.exports = {app: app};
+module.exports = {app: function(converter){
+    self.converter = converter;
+    return app;
+}};
--- test/e2e.js
+++ test/e2e.js
@@ -3,8 +3,9 @@ var request = require('supertest');
 var express = require('express');
 var expect = chai.expect; 
 var romanNumeralAPI = require('./../src/restapi');
+var romanNumeralConverter = require('./../src/converter');
 
-var app = romanNumeralAPI.app;
+var app = romanNumeralAPI.app(romanNumeralConverter);
 
 describe('Roman Numeral', function() {
   it('should return the roman numeral equivalent', function(done) {
