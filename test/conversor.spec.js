2cd303da2fcc32089ab64e5f3299a61694c48558
Alvaro Garcia <derecerca@gmail.com>
2016-07-22 15:38:59 +0100

    red: invoke conversor over the wire

---
+++ test/conversor.spec.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var chai = require('chai');
+var sinon = require('sinon');
+
+var demand = require("must");
+var nock = require("nock");
+var Conversor = require('./../src/conversor');
+var Request = require('./../src/request');
+
+
+describe("Conversor", function () {
+  var conversor;
+  
+  beforeEach(function () {
+    var request = Request();
+    conversor = Conversor(request);
+  });
+  
+  it("converts to roman", function (done) {
+    var arabic = '10';
+    nock('http://localhost').persist().post('/toroman').reply(200, {'roman': 'I'});
+    conversor.convertToRoman(arabic)
+      .then(function (response) {
+        var roman = response.roman;
+        demand(roman).must.be('I');
+        done();
+      });
+  });
+});
