891205ca62590242f22d58c8d1c03fa7989a1f1a
Alvaro Garcia <derecerca@gmail.com>
2016-07-23 01:09:57 +0100

    whitespace

--- src/conversor.js
+++ src/conversor.js
@@ -3,7 +3,8 @@
 module.exports = function (request) {
   return {
     convertToRoman: function (arabic) {
-      return request.post('http://localhost/toroman', {arabic: arabic})
+      return request
+        .post('http://localhost/toroman', {arabic: arabic})
         .then(function (response) {
           return response.roman;
         });
