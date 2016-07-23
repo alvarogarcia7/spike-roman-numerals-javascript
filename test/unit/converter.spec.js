8c87201b4d76edc562cb413c0262775100da9b5f
Alvaro Garcia <derecerca@gmail.com>
2016-07-23 01:45:48 +0100

    red: convert 10 to roman

--- test/unit/converter.spec.js
+++ test/unit/converter.spec.js
@@ -7,5 +7,6 @@ describe('Roman Numeral conversion', function () {
   describe('should return the roman numeral equivalent of ', function () {
     it('1', () => toRoman(1).should.equal('I'));
     it('5', () => toRoman(5).should.equal('V'));
+    it('10', () => toRoman(10).should.equal('X'));
   })
 });
