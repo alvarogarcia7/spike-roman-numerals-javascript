727490a9132a33ee190fd5d41a2003352d17bbc3
Alvaro Garcia <derecerca@gmail.com>
2016-07-23 01:44:30 +0100

    red: convert 5 to roman

--- test/unit/converter.spec.js
+++ test/unit/converter.spec.js
@@ -1,8 +1,11 @@
 var chai = require('chai');
 var should = require('should');
-var expect = chai.expect; 
+var expect = chai.expect;
 var toRoman = require('./../../src/converter');
 
-describe('Roman Numeral conversion', function() {
-  it('should return the roman numeral equivalent of 1', () => toRoman(1).should.equal('I'));
+describe('Roman Numeral conversion', function () {
+  describe('should return the roman numeral equivalent of ', function () {
+    it('1', () => toRoman(1).should.equal('I'));
+    it('5', () => toRoman(5).should.equal('V'));
+  })
 });
