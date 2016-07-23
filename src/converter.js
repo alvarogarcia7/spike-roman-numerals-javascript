e918cf03b392f8fb92a8bd1bbc2fd50e13d96891
Alvaro Garcia <derecerca@gmail.com>
2016-07-23 01:50:12 +0100

    refactor: generalize the way of converting to roman numeral (as long as it's 1,2,5,10)

--- src/converter.js
+++ src/converter.js
@@ -1,8 +1,15 @@
+var arabicToRoman = {
+  1: 'I',
+  2: 'II',
+  5: 'V',
+  10: 'X'
+};
+
 var converter = function (arabic) {
-  if (arabic === 1) return 'I';
-  if (arabic === 2) return 'II';
-  if (arabic === 10) return 'X';
-  return 'V';
+  if (arabic === 1) return arabicToRoman[arabic];
+  if (arabic === 2) return arabicToRoman[arabic];
+  if (arabic === 10) return arabicToRoman[arabic]
+  return arabicToRoman[arabic];
 };
 
 module.exports = converter;
