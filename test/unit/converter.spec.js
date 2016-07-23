def98a0b0afc61fa1f3a4d6f07071b1cc212093e
Alvaro Garcia <derecerca@gmail.com>
2016-07-23 01:43:04 +0100

    refactor: use inline function syntax

--- test/unit/converter.spec.js
+++ test/unit/converter.spec.js
@@ -4,7 +4,5 @@ var expect = chai.expect;
 var toRoman = require('./../../src/converter');
 
 describe('Roman Numeral conversion', function() {
-  it('should return the roman numeral equivalent of 1', function() {
-      toRoman(1).should.equal('I');
-  });
+  it('should return the roman numeral equivalent of 1', () => toRoman(1).should.equal('I'));
 });
