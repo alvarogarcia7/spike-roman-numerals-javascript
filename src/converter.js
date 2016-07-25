ff03717fd3114a9da6add5745ae06b0d90271695
Alvaro Garcia <derecerca@gmail.com>
2016-07-25 09:24:02 +0100

    green: convert to roman

--- src/converter.js
+++ src/converter.js
@@ -1,12 +1,35 @@
+"use strict";
+
 var arabicToRoman = {
-  1: 'I',
-  2: 'II',
   5: 'V',
+  1: 'I',
   10: 'X'
 };
 
 var converter = function (arabic) {
-  return arabicToRoman[arabic];
+  var remaining = arabic;
+  var values = Object.getOwnPropertyNames(arabicToRoman).sort((a, b) => {
+    return Number.parseInt(b) - Number.parseInt(a);
+  });
+  
+  var vals = [];
+  values.forEach(function (value) {
+    vals.push({arabic: value, roman: arabicToRoman[value]});
+  });
+  
+  values = vals;
+  
+  var roman = '';
+  var i = 0;
+  while (remaining > 0) {
+    while (remaining  >= values[i].arabic) {
+      roman += values[i].roman;
+      remaining -= values[i].arabic;
+    }
+    i++;
+  }
+  
+  return roman;
 };
 
 module.exports = converter;
